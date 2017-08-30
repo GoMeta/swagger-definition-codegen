600a8c5c675e9653c06078ff44f043871f54b869
Sean Thielen <sean@p27.us>
2017-08-30 18:21:20 -0400

    Updated comments

--- index.js
+++ index.js
@@ -16,11 +16,14 @@ if (program.args.length < 1) {
 	process.exit(1);
 }
 
+// Resolve paths
 const input = path.resolve(program.args[0]);
 const outputPath = path.resolve(program.output || '.');
 
+// Load the Swagger file
 const swagger = fs.readFileSync(input, 'utf8');
 
+// Attempt to parse JSON
 let definitions;
 try {
 	const parsedSwagger = JSON.parse(swagger);
@@ -31,25 +34,29 @@ try {
 }
 
 Object.keys(definitions)
-	.filter((definition) => definitions[definition].type === 'object')
+	.filter((definition) => definitions[definition].type === 'object') // Remove non-object definitions
 	.forEach((definition) => {
 		const properties = definitions[definition].properties;
 
 		const keys = Object.keys(properties);
 
+		// Map keys to javadoc params
 		const docs = keys
 			.map((key) => ` * @param ${key} ${properties[key].description.replace(/\n$/, '')}`)
 			.join('\n');
 
+		// Generate function arguments
 		const args = keys
 			.join(', ')
 			.trim();
 
+		// Generate explicit object map
 		const obj = keys
 			.map((key) => `    ${key}: ${key},`)
 			.join('\n')
 			.trim();
 
+		// Generate constructor code
 		const file = `
 /* eslint-disable object-shorthand, camelcase */
 
@@ -63,7 +70,8 @@ export default function ${definition}(${args}) {
   };
 }
 `;
-
+		
+		// Write the generated file
 		console.log(`Generating for ${definition}`);
 		fs.writeFileSync(path.join(outputPath, `${definition}.js`), file);
 	});
