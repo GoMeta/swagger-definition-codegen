1816429602a73105c8c48e1b29cf3e43af6fdc99
Yuval Peress <yuval.peress@gmail.com>
2017-09-10 21:38:26 -0400

    Add generated code for handlers

--- index.js
+++ index.js
@@ -1,59 +1,77 @@
 #!/usr/bin/env node
 
+const Handlers = require('./lib/handlers');
+
 const program = require('commander');
 const fs = require('fs');
+const fsExtra = require('fs-extra');
 const path = require('path');
 
 // Parse the command line arguments
 program
-	.version('1.0.0')
-	.usage('[options] <file ...>')
-	.option('-o, --output <output>', 'Specify the output directory')
-	.parse(process.argv);
+    .version('1.0.0')
+    .usage('[options] <file ...>')
+    .option('-o, --output <output>', 'Specify the output directory. Defaults to out')
+    .option('-d, --definitions-dir <definitions>', 'Specify the definitions directory. Defaults to definitions/')
+    .option('-h, --handlers-dir <handlers>', 'Specify the handlers directory. Defaults to handlers/')
+    .parse(process.argv);
 
 if (program.args.length < 1) {
-	console.error('You must supply an input.')
-	process.exit(1);
+    console.error('You must supply an input.');
+    process.exit(1);
 }
 
+const startTimestamp = Date.now();
+
 const input = path.resolve(program.args[0]);
-const outputPath = path.resolve(program.output || '.');
+const outputPath = path.resolve(program.output || 'out');
+const definitionsPath = path.join(outputPath, program.definitions || 'definitions');
+const handlersPath = path.join(outputPath, program.handlers || 'handlers');
+console.log(`input = ${input}`);
+console.log(`outputPath = ${outputPath}`);
+console.log(`definitionsPath = ${definitionsPath}`);
+console.log(`handlersPath = ${handlersPath}`);
 
 const swagger = fs.readFileSync(input, 'utf8');
 
 let definitions;
+let paths;
 try {
-	const parsedSwagger = JSON.parse(swagger);
-	definitions = parsedSwagger.definitions;
+    const parsedSwagger = JSON.parse(swagger);
+    definitions = parsedSwagger.definitions;
+    paths = parsedSwagger.paths;
 } catch (err) {
-	console.error('Your Swagger file must be valid JSON.');
-	process.exit(1);
+    console.error('Your Swagger file must be valid JSON.');
+    process.exit(1);
 }
 
+console.log("Generating definitions...");
+fsExtra.ensureDirSync(definitionsPath);
 Object.keys(definitions)
-	.filter((definition) => definitions[definition].type === 'object')
-	.forEach((definition) => {
-		const properties = definitions[definition].properties;
+    .filter((definition) => definitions[definition].type === 'object')
+    .forEach((definition) => {
+        const properties = definitions[definition].properties;
 
-		const keys = Object.keys(properties);
+        const keys = Object.keys(properties);
 
-		const docs = keys
-			.map((key) => {
-				const description = properties[key].description || '';
-				return ` * @param ${key}${description.replace(/\n$/, '')}`;
-			})
-			.join('\n');
+        const docs = keys
+            .map((key) => {
+                const description = properties[key].description || '';
+                return ` * @param ${key}${description.replace(/\n$/, '')}`;
+            })
+            .join('\n');
 
-		const args = keys
-			.join(', ')
-			.trim();
+        const args = keys
+            .join(', ')
+            .trim();
 
-		const obj = keys
-			.map((key) => `    ${key}: ${key},`)
-			.join('\n')
-			.trim();
+        const obj = keys
+            .map((key) => `    ${key}: ${key},`)
+            .join('\n')
+            .trim();
 
-		const file = `/* eslint-disable */
+        const file =
+`/* eslint-disable */
 
 /**
  * @name ${definition}
@@ -65,8 +83,8 @@ export default class ${definition} {
    *
    * @memberOf ${definition}
 ${keys.map((key) => {
-	const description = properties[key].description || '';
-	return `   * @param ${key} ${description.replace(/\n$/, '')}`
+    const description = properties[key].description || '';
+    return `   * @param ${key} ${description.replace(/\n$/, '')}`
 }).join('\n')}
    */
   constructor(${keys.join(', ').trim()}) {
@@ -113,6 +131,12 @@ ${keys.map((key) => `      ${key}: this.${key},`).join('\n')}
   }
 }
 `;
-		console.log(`Generating for ${definition}`);
-		fs.writeFileSync(path.join(outputPath, `${definition}.js`), file);
-	});
+        process.stdout.clearLine();
+        process.stdout.write(`\rGenerating for ${definition}`);
+        fs.writeFileSync(path.join(definitionsPath, `${definition}.js`), file);
+    });
+
+process.stdout.clearLine();
+process.stdout.cursorTo(0);
+Handlers.printHandlers(paths, handlersPath);
+console.log(`Finished in ${(Date.now() - startTimestamp)/1000.0} seconds`);
